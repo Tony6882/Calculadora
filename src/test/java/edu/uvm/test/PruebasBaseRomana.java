96d86ae8a8dd026afe22f52a21ba9dc3b81a974d
contac19 <contac19@my.uvm.edu.mx>
2025-05-20 17:43:10 -0600

    Arreglado

--- src/test/java/edu/uvm/test/PruebasBaseRomana.java
+++ src/test/java/edu/uvm/test/PruebasBaseRomana.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import edu.uvm.ConversorNumeroRomano;
 @DisplayName("Pruebas del Conversor de Números Romanos (con negativos)")
 class ConversorNumeroRomanoTest {
 
